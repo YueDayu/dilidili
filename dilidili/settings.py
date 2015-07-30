6edb271043cf1222f157300e042e2563023f2f20
岳大禹 <yuedayu13@gmail.com>
2015-07-30 13:26:28 +0800

    database: use mySQL

--- dilidili/settings.py
+++ dilidili/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'dilidili.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'dilidili.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "dilidili",
+        "USER": "root",
+        "PASSWORD": "YOUR_PASSWORD",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
