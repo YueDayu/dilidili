bf9a104a72838f80b3c9c4042bcca6427f6c5ced
岳大禹 <yuedayu13@gmail.com>
2015-07-31 03:45:37 +0800

    url: 配置媒体文件路径

--- dilidili/urls.py
+++ dilidili/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import dilidili.settings as settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_URL}),
 ]
