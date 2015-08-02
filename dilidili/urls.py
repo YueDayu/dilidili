6afd5805b16f8ee16872d23b646c30c65d450882
qwertyaqwerty <gracetw100630@gmail.com>
2015-08-03 06:30:38 +0800

    view: init search

--- dilidili/urls.py
+++ dilidili/urls.py
@@ -29,5 +29,7 @@ urlpatterns = [
     url(r'^upload-photo/$', 'dilidili_dev.upload_photo.upload_photo'),
     url(r'^upload-success/$', 'dilidili_dev.upload_photo.upload_success'),
     url(r'^process-photo/$', 'dilidili_dev.upload_photo.process_img', name='process-photo'),
+    url(r'^search/$', 'dilidili_dev.search.search_mainpage'),
+    url(r'^search/request/$', 'dilidili_dev.search.search'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 ]
