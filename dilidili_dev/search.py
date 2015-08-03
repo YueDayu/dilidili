1cc971248b989b2234402297c4eb5971afe05491
岳大禹 <yuedayu13@gmail.com>
2015-08-04 06:28:02 +0800

    code： reformat the code.

--- dilidili_dev/search.py
+++ dilidili_dev/search.py
@@ -5,23 +5,23 @@ from django.views.decorators.http import require_http_methods
 from dilidili_dev.models import *
 
 
-#for ajax GET, return JSON
+# for ajax GET, return JSON
 @require_http_methods(["GET"])
 def search(request):
-	data = request.GET
-	objects = Video.objects.all()
-	if "filters" in data:
-		filters_tmp = QueryDict(data["filters"])
-		for field in filters_tmp:
-			field_new = field if "__" in field else field + "__iexact"
-			objects = objects.filter(**{field_new:filters_tmp.get(field)})
-	sortby = data["order_by"] if "sortby" in data else "?"
-	offset = int(data["offset"] if "offset" in data else "0")
-	limitto = int(data["limit_to"] if "limit_to" in data else "100")
-	objects = objects.order_by(sortby)[offset : offset+limitto]
-	return JsonResponse(serializers.serialize('json', objects), safe=False)
+    data = request.GET
+    objects = Video.objects.all()
+    if "filters" in data:
+        filters_tmp = QueryDict(data["filters"])
+        for field in filters_tmp:
+            field_new = field if "__" in field else field + "__iexact"
+            objects = objects.filter(**{field_new: filters_tmp.get(field)})
+    sortby = data["order_by"] if "sortby" in data else "?"
+    offset = int(data["offset"] if "offset" in data else "0")
+    limitto = int(data["limit_to"] if "limit_to" in data else "100")
+    objects = objects.order_by(sortby)[offset: offset + limitto]
+    return JsonResponse(serializers.serialize('json', objects), safe=False)
 
 
 @require_http_methods(["GET"])
 def search_mainpage(request):
-	return render(request, 'search/search.html')
+    return render(request, 'search/search.html')
