cfa26da76b77182200ecc0952a34707cd71618fc
qwertyaqwerty <gracetw100630@gmail.com>
2015-08-05 05:41:15 +0800

    settings: fix /admin

--- dilidili/settings.py
+++ dilidili/settings.py
@@ -122,3 +122,5 @@ from easy_thumbnails.conf import Settings as thumbnail_settings
 THUMBNAIL_PROCESSORS = (
     'image_cropping.thumbnail_processors.crop_corners',
 ) + thumbnail_settings.THUMBNAIL_PROCESSORS
+
+SITE_ID = 1
