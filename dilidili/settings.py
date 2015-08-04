0e9582c419d0631508303601819b5f561a1f26e9
qwertyaqwerty <gracetw100630@gmail.com>
2015-08-05 04:52:50 +0800

    admin: add video admin

--- dilidili/settings.py
+++ dilidili/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'easy_thumbnails',
     'image_cropping',
     'dilidili_dev',
