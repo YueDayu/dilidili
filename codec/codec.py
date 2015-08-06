85ab35f7b3e86fa44c243bd88635831646710862
qwertyaqwerty <gracetw100630@gmail.com>
2015-08-06 12:34:32 +0800

    codec: add screenshot

--- codec/codec.py
+++ codec/codec.py
@@ -22,7 +22,16 @@ if __name__ == "__main__":
 				if os.system("ffmpeg -i %s -b:v 584k %s" % (video_full_path, video_full_name)) == 0:
 					cursorU.execute("UPDATE dilidili_dev_video SET video='%s', status=1 WHERE id=%u;" % (video_name, pk))
 			except os.error:
-				pass
+				continue
+
+			if image == "photos/001.jpg":
+				image_name = video + ".screenshot.jpg"
+				image_full_path = (media_root + image_name).replace('/', '\\')
+				try:
+					if os.system("ffmpeg -ss 20 -i %s -frames 1 -qscale 1 %s" % (video_full_path, image_full_path)) == 0:
+						cursorU.execute("UPDATE dilidili_dev_video SET image='%s' WHERE id=%u;" % (image_name, pk))
+				except os.error:
+					continue
 		else:
 			print("No new video added")
 		cnx.commit()
